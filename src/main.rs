2ec321dc0b1048e6eefbfec6819c9b0b602ccf13
Ivy Wong <ivy@ivywong.dev>
2024-02-03 17:00:04 -0800

    [6] add red sphere

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::io::{stderr, Write};
 use vec::{Color, Vec3, Point3};
 
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
     // looking at y after normalizing it causes horizontal gradient: why?
     let unit_direction = r.direction().normalized();
     let t = 0.5 * (unit_direction.y() + 1.0);
@@ -13,6 +16,15 @@ fn ray_color(r: &Ray) -> Color {
     // linear interpolation
     // start from white and go to blue
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
 }
 
 fn main() {
